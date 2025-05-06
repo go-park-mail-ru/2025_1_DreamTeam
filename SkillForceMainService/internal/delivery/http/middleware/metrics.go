1e2a0d93c9290512315f7985f40cf2d4a96302bb
Ivan-Skorokhodov <dreikonline5554545@gmail.com>
2025-05-06 18:32:22 +0300

    fix bug in metrics in main-service

--- SkillForceMainService/internal/delivery/http/middleware/metrics.go
+++ SkillForceMainService/internal/delivery/http/middleware/metrics.go
@@ -8,6 +8,35 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var ValidPaths = map[string]bool{
+	"/api/register":                   true,
+	"/api/login":                      true,
+	"/api/logout":                     true,
+	"/api/isAuthorized":               true,
+	"/api/updateProfile":              true,
+	"/api/updateProfilePhoto":         true,
+	"/api/deleteProfilePhoto":         true,
+	"/api/validEmail":                 true,
+	"/api/getCourses":                 true,
+	"/api/searchCourses":              true,
+	"/api/getCourse":                  true,
+	"/api/getCourseLesson":            true,
+	"/api/getNextLesson":              true,
+	"/api/markLessonAsNotCompleted":   true,
+	"/api/getCourseRoadmap":           true,
+	"/api/video":                      true,
+	"/api/createCourse":               true,
+	"/api/addCourseToFavourites":      true,
+	"/api/deleteCourseFromFavourites": true,
+	"/api/getFavouriteCourses":        true,
+	"/api/GetTestLesson":              true,
+	"/api/AnswerQuiz":                 true,
+	"/api/GetQuestionTestLesson":      true,
+	"/api/AnswerQuestion":             true,
+	"/api/docs/":                      true,
+	"/metrics":                        true,
+}
+
 var (
 	httpRequestsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -40,6 +69,11 @@ func MetricsMiddleware(next http.Handler) http.Handler {
 
 		duration := time.Since(start).Seconds()
 		path := r.URL.Path
+
+		if _, ok := ValidPaths[path]; !ok {
+			return
+		}
+
 		method := r.Method
 		status := strconv.Itoa(recorder.statusCode)
 
